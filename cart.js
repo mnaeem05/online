519d9384078a051f14d5b49a2bb0ba7456c86b53
Naeem <Naeem>
2017-06-07 10:39:01 +0500

    Online shopping site 07-june

---
+++ cart.js
@@ -0,0 +1,24 @@
+var nameArray = [];
+
+var itemAdd = 0;
+function incrementItem1() {
+    itemAdd++;
+alert( (itemAdd) + " Item Added")
+}
+
+var itemLess = 0;
+function decrementItem1() {
+    itemLess++;
+    alert((itemLess) + " Item Removed")
+    // document.getElementsByClassName("addCart").innerHTML+="<p> "itemLess" hh </p>"
+}
+
+
+// var itemLess = 0;
+// function decrementItem1() {
+//     itemLess++;
+//     var userInput1 = document.getElementsByClassName("button1").innerHTML;
+//         // document.getElementById ("tableId").innerHTML = "<> Name: "+userInput1+" </h4>"
+//         console.log( "tt" + itemLess)
+//         document.getElementsByClassName("addCart").innerHTML = "<p> Name:"+itemLess+" </p>";
+// }
