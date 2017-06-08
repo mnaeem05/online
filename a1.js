467216afd1db0a6193d62ce2c8a2ac1ee5442cf9
Naeem <Naeem>
2017-06-08 17:53:57 +0500

    Changes 08-june

--- a1.js
+++ a1.js
@@ -1,7 +1,27 @@
 var itemAdd = 0;
 function incrementItem1() {
     itemAdd++;
+    document.getElementById("box01").innerHTML;
+    if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("lastname", itemAdd)
+    // Retrieve
+    document.getElementById("final").innerHTML = localStorage.getItem("lastname");
+} else {
+    document.getElementById("final").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
+    // itemAdd++;
 document.getElementById("box01").innerHTML;
-document.getElementById("final").innerHTML += "<h3> Item Added "+itemAdd+" </h3>"
+// document.getElementById("final").innerHTML = "<h3> Item Added "+itemAdd+" </h3>"
 // alert( (itemAdd) + " Item Added")
+}
+
+
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("lastname", "  one")
+    // Retrieve
+    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+} else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
--- cart.js
+++ cart.js
@@ -9,17 +9,12 @@ document.getElementById("final").innerHTML= "<h5> Item Added "+itemAdd+" </h5>"
 var itemLess = 0;
 function decrementItem1() {
     itemLess++;
+    if(itemAdd >= itemLess){
     document.getElementById("box002").innerHTML;
 document.getElementById("final2").innerHTML= "<h5> Item Removed "+itemLess+" </h5>"
     // alert((itemLess) + " Item Removed")
+    }
+    else{
+        document.getElementById("final2").innerHTML= "<h5> No Item Added </h5>"
+    }
 }
-
-
-// var itemLess = 0;
-// function decrementItem1() {
-//     itemLess++;
-//     var userInput1 = document.getElementsByClassName("button1").innerHTML;
-//         // document.getElementById ("tableId").innerHTML = "<> Name: "+userInput1+" </h4>"
-//         console.log( "tt" + itemLess)
-//         document.getElementsByClassName("addCart").innerHTML = "<p> Name:"+itemLess+" </p>";
-// }
